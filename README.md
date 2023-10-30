b75156c91bcdb2f05c8eb88fcf1cff90b17cbed9
LenaAli56 <149244590+LenaAli56@users.noreply.github.com>
2023-10-30 21:17:09 +0300

    assignment1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python008
+Welcome to Python 008 class
  we will learn   about python
+ 
---
+++ classes/assignment 1.py
@@ -0,0 +1,23 @@
+page={'title1':'Kingdom of Jordan'}
+page1={'title2':'Egypation Republic'}
+page2={ 'welcom':'Welcome to Kingdom of Jordan page',
+        'subject':'Jordan is an Arab Islamic country,it is located on the continent of Asia,its capital is Amman'}
+page3={ 'welcome':'Welcome to Egypation Republic page',
+        'subject':'Egypt is an Arab Islamic country,it is located on the continent of Africa ,its capital is Cairo'}
+for i in page:
+    print(page[i])
+    for j in page2:
+        print(' ',page2[j])
+    for n in page1:
+        print(page1[n])
+    for o in page3:
+        print(' ',page3[o])
+
+
+
+# page1={'Title':'Kingdom of Jordan',
+#        'welcom':'Welcome to Kingdom of Jordan page',
+#        'subject':'Jordan is an Arab Islamic country,it is located on the continent of Asia,its capital is Amman'}
+# page2={'Title':'Egypation Republic',
+#        'welcome':'Welcome to Egypation Republic page',
+#        'subject':'Egypt is an Arab Islamic country,it is located on the continent of Africa ,its capital is Cairo'}
