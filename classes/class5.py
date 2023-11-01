22801453f5e2e36fafbe1331c5f20d88bfe837ca
LenaAli56 <149244590+LenaAli56@users.noreply.github.com>
2023-11-01 15:58:00 +0300

    main

---
+++ classes/class5.py
@@ -0,0 +1,13 @@
+# i = 0
+# whiletest=True
+# while whiletest:
+#     if i==5:
+#          i+=1
+#         continue#اي شي بعده لا  ينفذ في loop
+#     print(i)
+#      i+=1
+#     if i==10:
+#         whiletest=False#or use break
+
+# while:
+#     if
--- main.py
+++ main.py
@@ -1,1 +1,38 @@
- #python
+ #python 
+allperson=[]
+person={}
+print("Welcom to our app")
+while True:
+    print("_"*20)
+    print("1-Add new person")
+    print("2- see all persons")
+    print("3- exit")
+    choise=input("Enter your choise : ")
+    if choise =="1":
+        name=input("Enter the person name : ")
+        age=input("Enter the person age : ")
+        if age == (type.str):
+            for i in age:
+                print("Enter the age person agena")
+        age=int(age)
+        job=input("Enter the person job : ")
+        person['name']=name
+        person['age']=age
+        person['job']=job
+        allperson.append(person)
+        print("Add your person successfully")
+    elif choise =="2":
+        counter=1
+        print('_'*20)
+        print('all persons : ')
+        for i in allperson:
+            print("person",':',counter)
+            for key in i:
+                print('     ',key,':',i[key])
+            counter+=1
+    else:
+        print("Thanks")
+        break
+                
+
+  
