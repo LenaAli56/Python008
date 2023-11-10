b809a95c454e9670954e8fed14409d71904a61e2
LenaAli56 <149244590+LenaAli56@users.noreply.github.com>
2023-11-10 18:08:53 +0300

    class1

--- main.py
+++ main.py
@@ -10,13 +10,18 @@ while True:
     choise=input("Enter your choise : ")
     if choise =="1":
         name=input("Enter the person name : ")
-        # while True:
-        #     age=input("Enter the person age : ")
-        #     if age.isdigit()==False:
-        #         print("Please enter valid age : ")
-        #         continue
-        #     else :
-        #         break
+        if allperson !=[]:
+            isExited=True
+            while True:
+                for i in allperson:
+                    #i =1 person as dic
+                    if name ==i['name']:
+                        print("this name is already exist")
+                        name=input('enter the user name : ')
+                        isExited=True
+                        break
+                    else:
+                        isExited=False
         while True:
             try:
                 age=input("Enter the person age : ")
@@ -25,9 +30,21 @@ while True:
                 print("Please enter valid age : ")
                 
         job=input("Enter the person job : ")
+        skills=[]
+        while True:
+            try:
+                counter2=input('enter the number of skills')
+                break
+            except Exception as e:
+                print('please enter a void number ')
+                continue
+        for i in range(counter2):
+            print('enter the skill number',i+1,'; ')
+            skills.append(input)
         person['name']=name
         person['age']=age
         person['job']=job
+        person['skills']=skills
         allperson.append(person)
         print("Add your person successfully")
     elif choise =="2":
@@ -42,6 +59,11 @@ while True:
     else:
         print("Thanks")
         break
-                
+    
+ 
+
+
+
+
 
   
