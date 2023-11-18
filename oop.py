bbb666654cbe1dc7d1381d4d1f2b858d5f2bf69a
LenaAli56 <149244590+LenaAli56@users.noreply.github.com>
2023-11-18 14:02:35 +0300

    oop

---
+++ oop.py
@@ -0,0 +1,13 @@
+# class Clinet:
+#     name="Lena"
+#     age=19
+#     city="Irbid"
+
+# clinet1=Clinet()
+# print(clinet1.name)
+
+
+#class has attributes and methods
+#what is the difference between atributes and methods
+###attributes : are varibales
+###method : are functions
