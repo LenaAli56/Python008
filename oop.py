d758e44cb108081a0e42822b84cbb536c5320062
LenaAli56 <149244590+LenaAli56@users.noreply.github.com>
2023-11-24 19:53:50 +0300

    class Person

--- oop.py
+++ oop.py
@@ -11,3 +11,40 @@
 #what is the difference between atributes and methods
 ###attributes : are varibales
 ###method : are functions
+
+
+class Person ():
+    def __init__(self,name,age,job,skills,password):
+        self.name = name
+        self.age = age
+        self.job = job
+        self.skills = skills
+        self.password=password
+
+
+    def nameAndAge(self):
+     return f'name : {self.name} , age : {self.age}'
+    
+    def callPerson(self,person):
+       return f'{self.name} is calling {person}'
+    
+    def __str__(self):
+     return f"name : {self.name} , age : {self.age} , job : {self.job} , skills : {self.skills} "
+
+person1=Person(age=19,skills=['python','java','js'],name='lena',job='developer')
+person2=Person(age=20,skills=['java','js'],name='ali',job='developer')
+print(person1)
+print(person2.callPerson('Ziad'))
+print(person2.nameAndAge())
+        
+
+
+
+
+
+
+
+
+
+
+
